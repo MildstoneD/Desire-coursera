04cd8f9033cb01da853398272da6b6680ac4d85e
MildstoneD <98883461+MildstoneD@users.noreply.github.com>
2022-06-23 21:46:06 +0100

    Update script.js

--- soccer Lounge/home page/script.js
+++ soccer Lounge/home page/script.js
@@ -3,7 +3,7 @@ var images = ['./images/pexels-pixabay-209841.jpg',
     './images/pexels-gonzalo-acuña-10923063.jpg',
     './images/pexels-tom-fisk-large.jpg'
 ];
-var slideTimer = 9000;
+var slideTimer = 15000;
 
 function changePicture() {
     document.getElementById('navmain').style.background = 'url(' + images[i] + ')';
