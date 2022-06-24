3537e53f33f39cf2f691ff9f7d5bcbb989ca9fcd
MildstoneD <98883461+MildstoneD@users.noreply.github.com>
2022-06-24 21:38:59 +0100

    Update script.js

--- soccer Lounge/home page/script.js
+++ soccer Lounge/home page/script.js
@@ -1,20 +1,20 @@
-var i = 0;
-var images = ['./images/pexels-pixabay-209841.jpg',
-    './images/pexels-gonzalo-acuña-10923063.jpg',
-    './images/pexels-tom-fisk-large.jpg'
-];
-var slideTimer = 15000;
+// var i = 0;
+// var images = ['./images/pexels-pixabay-209841.jpg',
+//     './images/pexels-gonzalo-acuña-10923063.jpg',
+//     './images/pexels-tom-fisk-large.jpg'
+// ];
+// var slideTimer = 15000;
 
-function changePicture() {
-    document.getElementById('navmain').style.background = 'url(' + images[i] + ')';
+// function changePicture() {
+//     document.getElementById('navmain').style.background = 'url(' + images[i] + ')';
 
-    if (i < images.length - 1) {
-        i++;
-    } else {
-        i = 0;
-    }
-    setTimeout(changePicture, slideTimer);
+//     if (i < images.length - 1) {
+//         i++;
+//     } else {
+//         i = 0;
+//     }
+//     setTimeout(changePicture, slideTimer);
 
-}
+// }
 
-window.onload = changePicture;
+// window.onload = changePicture;
