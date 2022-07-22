e02f131ced1b10a1085228b585b1e51d01625f94
MildstoneD <98883461+MildstoneD@users.noreply.github.com>
2022-07-22 21:28:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -37,24 +37,24 @@ hamburg.addEventListener('click', function() {
     }
 });
 
-// javascript for tab and tabcontent
-function openTab (evt, tabName) {
-var i, tabcontent, tablinks;
+function openCity(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
 
-// hide tab
-tabcontent = document.getElementsByClassName("tabcontent");
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = 'none';
-}
-//exit active tab
-tablinks = document.getElementsByClassName("tablinks");
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
-}
+    }
 
-//show current tab
-document.getElementById(tabName).style.display = 'block';
-    evt.currentTarget.tabName += " active"; 
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
-//default tab
 document.getElementById("defaultTab").click();
